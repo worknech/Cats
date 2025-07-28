b2207e31d1a676711bec25211d35fdc6b8104779
worknech <worknech@yandex.ru>
2025-07-28 22:20:01 +0300

    Загрузка изображения в новом окне

--- cataas.py
+++ cataas.py
@@ -10,31 +10,47 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f'Произошла ошибка: {e}')
         return None
 
 
-def set_image():
+def open_new_window():
     img = load_image(url)
-
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title('Картинка с котиком')
+        new_window.geometry('600x480')
+        label = Label(new_window, image=img)
+        label.pack()
         label.image = img
+
+
+def exit():
+    root.destroy()
 
 
 root = Tk()
 root.title('Cats!')
-root.geometry('600x480')
+root.geometry('600x520')
 
-label = Label()
-label.pack()
 
-update_button = Button(text='Обновить', command=set_image)
-update_button.pack()
+
+# update_button = Button(text='Обновить', command=set_image)
+# update_button.pack()
+
+menu_bar = Menu(root)
+root.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='File', menu=file_menu)
+file_menu.add_command(label='Загрузить фото', command=open_new_window)
+file_menu.add_separator()
+file_menu.add_command(label='Выход', command=exit)
 
 url = 'https://cataas.com/cat'
-set_image()
+# open_new_window()
 
 root.mainloop()
