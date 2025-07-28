2648ad0a7454c92b30273f01aa21ceb4268b6fd3
worknech <worknech@yandex.ru>
2025-07-28 18:36:17 +0300

    Создано окно и виджеты

--- cataas.py
+++ cataas.py
@@ -1,1 +1,23 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+def load_image():
+    pass
+
+root = Tk()
+root.title('Cats!')
+root.geometry('600x480')
+
+label = Label()
+label.pack()
+
+url = 'https://cataas.com/cat'
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+root.mainloop()
