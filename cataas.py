90a14fd40595643b0aef5fc47fa44eaac85a3bf1
worknech <worknech@yandex.ru>
2025-07-29 15:42:19 +0300

    Добавлено получение изображений по тегу и без тега

--- cataas.py
+++ cataas.py
@@ -18,7 +18,9 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
+    img = load_image(url_tag)
     if img:
         new_window = Toplevel()
         new_window.title('Картинка с котиком')
@@ -36,10 +38,11 @@ root = Tk()
 root.title('Cats!')
 root.geometry('600x520')
 
+tag_entry = Entry()
+tag_entry.pack()
 
-
-# update_button = Button(text='Обновить', command=set_image)
-# update_button.pack()
+load_button = Button(text='Загрузить по тегу', command=open_new_window)
+load_button.pack()
 
 menu_bar = Menu(root)
 root.config(menu=menu_bar)
